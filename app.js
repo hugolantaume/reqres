058da5027bb485c939168068792b3d9ceddff4ac
Ben Howdle <benhowdle89@gmail.com>
2014-10-16 18:04:43 +0100

    dns

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 
 app.get("/", function(req, res) {
 	res.send("Hello World!");
