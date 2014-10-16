39d9a769a62856d0b67724531bd3cd77f2bb7971
Ben Howdle <benhowdle89@gmail.com>
2014-10-16 21:56:42 +0100

    layout and 404

--- app.js
+++ app.js
@@ -33,6 +33,26 @@ app.get("/", function(req, res, next) {
 
 app.get("/api/:resource/*", routes.get);
 
+app.use(function(req, res, next) {
+	res.status(404);
+
+	if (req.accepts("html")) {
+		res.render("404.html", {
+			url: req.url
+		});
+		return;
+	}
+
+	if (req.accepts("json")) {
+		res.send({
+			error: "Not found"
+		});
+		return;
+	}
+
+	res.type("txt").send("Not found");
+});
+
 var server = app.listen(port, function() {
 
 	var host = server.address().address,
