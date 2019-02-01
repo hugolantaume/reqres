93982170ddafb3343719ba1906b00a2ce27c7768
Ankit Goyal <ankit.goyal@hackerrank.com>
2019-02-01 16:55:54 +0530

    Move helper functions out of index route

---
+++ routes/helper.js
@@ -0,0 +1,69 @@
+var config = require("./../config.json"),
+	_ = require("lodash");
+
+module.exports = {
+    returnAll: function(items, req, res, options={}) {
+        var page = req.query.page || 1,
+            offset = (page - 1) * (options.page_size || config.pagination.page_size),
+            paginatedItems = _.rest(items, offset).slice(0, (options.page_size || config.pagination.page_size));
+    
+        return res.status(200).send({
+            page: page,
+            per_page: options.page_size || config.pagination.page_size,
+            total: items.length,
+            total_pages: Math.ceil(items.length / (options.page_size || config.pagination.page_size)),
+            data: paginatedItems
+        });
+    },
+    
+    returnSingle: function(items, itemArg, res) {
+        var singleItem = items.filter(function(item) {
+            return item.id == itemArg;
+        });
+        if (singleItem.length) {
+            return res.status(200).send({
+                data: singleItem[0]
+            });
+        }
+        return res.status(404).send({});
+    },
+    
+    search: function(items, req, res, exactMatch=false) {
+        filteredItems = items;
+        filters = getFilters(items, req, res);
+        _.each(filters, function(value, key) {
+            if (exactMatch) {
+                filteredItems = _.filter(filteredItems, function(item) {
+                    return (getSanitizedValue(item[key]) === getSanitizedValue(value));
+                });
+            } else {
+                filteredItems = _.filter(filteredItems, function(item) {
+                    return getSanitizedValue(item[key]).indexOf(getSanitizedValue(value)) > -1;
+                });
+            }
+        });
+        return filteredItems;
+    }
+};
+
+function getFilters(items, req, res) {
+    allowedKeys = _.keys(items[0]);
+    excludedQueryParams = ['page'];
+    filters = {};
+    _.each(req.query, function(value, key) {
+        if (_.indexOf(excludedQueryParams, key) > -1) {
+            //nothing to be done
+        } else if (_.indexOf(allowedKeys, key) > -1) {
+            filters[key] = value;
+        }
+    });
+    return filters;
+}
+
+function getSanitizedValue(val) {
+    if (["number", "boolean"].indexOf(typeof(val)) !== -1) {
+        return val.toString();
+    }
+    
+    return val.toLowerCase();
+}
